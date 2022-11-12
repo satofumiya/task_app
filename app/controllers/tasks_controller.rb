9de5a01cb68c3ff5798f80fea9322affa11b155d
satofumiya <sato.workfree@gmail.com>
2022-11-12 14:17:13 +0900

    新規編集画面と確認画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,24 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(params.require(:task).permit(:title,:start,:end,:all_day,:memo))
+    if @task.save
+      flash[:notice] = "スケジュールを新規登録しました。"
+      redirect_to :tasks
+    else
+      render "new"
+    end
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
