b3884fc29a106d0771449e06bb327ce6d68848c8
satofumiya <sato.workfree@gmail.com>
2022-11-13 18:04:58 +0900

    スケジュール管理アプリ完成形

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params.require(:task).permit(:title,:start,:end,:all_day,:memo))
+    @task = Task.new(task_param)
     if @task.save
-      flash[:notice] = "スケジュールを新規登録しました。"
+
       redirect_to :tasks
+      flash[:notice] = "スケジュールを新規登録しました。"
     else
+      flash[:notice] = "スケジュールを登録できませんでした。"
       render "new"
     end
   end
@@ -22,11 +24,30 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_param)
+      flash[:notice] = "スケジュール情報を更新しました"
+      redirect_to :tasks
+    else
+      flash[:notice] = "スケジュール情報を更新できませんでした。"
+      render "edit"
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:notice] = "ユーザーを削除しました"
+    redirect_to :tasks
   end
+
+  private
+    def task_param
+      params.require(:task).permit(:title,:start,:end,:all_day,:memo)
+    end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+    validates :title, presence: true
+    validates :start, presence: true
+    validates :end, presence: true
+    validates :memo, presence: true
 end
